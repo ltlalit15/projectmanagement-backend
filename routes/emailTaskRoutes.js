889577959558f65c4970b45d3e94944e89e3d86f
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-08-01 18:47:42 +0530

    Update emailTaskRoutes.js

--- routes/emailTaskRoutes.js
+++ routes/emailTaskRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createEmailTask, getAllEmailTasks, getEmailTaskById, updateEmailTask, deleteEmailTask } = require('../controller/emailTask');
+const { createEmailTask, getAllEmailTasks, getEmailTaskById, getEmailTaskByUserId, updateEmailTask, deleteEmailTask } = require('../controller/emailTask');
 const authMiddleware = require('../middleware/authMiddleware');
 const multer = require('multer');
 const path = require('path');
@@ -10,6 +10,7 @@ const router = express.Router();
 router.post('/createEmailTask', authMiddleware, createEmailTask);
 router.get('/getAllEmailTasks', getAllEmailTasks);
 router.get('/getEmailTaskById/:id', getEmailTaskById);
+router.get('/getEmailTaskByUserId/:assignedTo', getEmailTaskByUserId);
 router.patch('/updateEmailTask/:id', authMiddleware, updateEmailTask);
 router.delete('/deleteEmailTask/:id', authMiddleware, deleteEmailTask);
 
