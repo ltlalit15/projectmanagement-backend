a9b59dbbb923b225e1b10c2219c1f3a2d2a92afd
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-08-01 18:37:38 +0530

    Update employeeTaskRoutes.js

--- routes/employeeTaskRoutes.js
+++ routes/employeeTaskRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addEmployeeTask, getAllEmployeeTasks, getEmployeeTaskById, updateEmployeeTask, deleteEmployeeTask } = require('../controller/employeeTask');
+const { addEmployeeTask, getAllEmployeeTasks, getEmployeeTaskById, getEmployeeTaskByUserId, updateEmployeeTask, deleteEmployeeTask } = require('../controller/employeeTask');
 const authMiddleware = require('../middleware/authMiddleware');
 const multer = require('multer');
 const path = require('path');
@@ -10,6 +10,7 @@ const router = express.Router();
 router.post('/addEmployeeTask', authMiddleware, addEmployeeTask);
 router.get('/getAllEmployeeTasks', getAllEmployeeTasks);
 router.get('/getEmployeeTaskById/:id', getEmployeeTaskById);
+router.get('/getEmployeeTaskByUserId/:assignedTo', getEmployeeTaskByUserId);
 router.patch('/updateEmployeeTask/:id', authMiddleware, updateEmployeeTask);
 router.delete('/deleteEmployeeTask/:id', authMiddleware, deleteEmployeeTask);
 
